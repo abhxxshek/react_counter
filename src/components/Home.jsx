cb55f39354067291b91488e68b9fb46dc62e889d
abhxxshek <abhishekanil2007@gmail.com>
2025-01-23 11:59:48 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
 
   return (
     <>
-    <h1>Welcome {name} to MERN learning</h1>
+    <h1>Welcome {name} to MERN Learning</h1>
     <Button variant="outlined" onClick={changeValue}>CHANGE</Button> 
     <Button variant="outlined" onClick={changeCount}>CHANGE COUNT</Button> {/* */}
     <small>button press{count1} times</small><br /><br />
